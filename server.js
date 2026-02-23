5d33215839cf2e5bd5c59d592c968eface98c89f
Cristian camilo <cristianmilo07@gmail.com>
2026-02-23 15:03:48 -0500

    Se organiza código faltante

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const path = require('path');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const historiasRoutes = require('./routes/historias');
